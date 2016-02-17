efb23fef8320e65979661b14e813d80691d389d6
Choi, Jaehee <jaehee-choi@rsupport.com>
2016-02-17 20:30:12 +0900

    #IMPROVED https://egghead.io/lessons/react-using-refs-to-access-components Example

--- ex15_es6_hello_world/app/components/App.js
+++ ex15_es6_hello_world/app/components/App.js
@@ -1,31 +1,52 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 
 class App extends React.Component {
   constructor() {
     super();
     this.state = {
-      txt: 'this is state'
+      red: 0,
+      green: 0,
+      blue: 0
     }
     this.update = this.update.bind(this);
   }
   update(e) {
     this.setState({
-      txt: e.target.value
+      red: ReactDOM.findDOMNode(this.refs.red.refs.inp).value,
+      green: ReactDOM.findDOMNode(this.refs.green.refs.inp).value,
+      blue: ReactDOM.findDOMNode(this.refs.blue.refs.inp).value
     })
   }
   render() {
     return (
       <div>
-        <Widget txt={this.state.txt} update={this.update} />
-        <Widget txt={this.state.txt} update={this.update} />
-        <Widget txt={this.state.txt} update={this.update} />
-        <Widget txt={this.state.txt} update={this.update} />
+        <Slider ref="red" update={this.update} />
+        <h1>{this.state.red}</h1>
+        <hr />
+        <Slider ref="green" update={this.update} />
+        <h1>{this.state.green}</h1>
+        <hr />
+        <Slider ref="blue" update={this.update} />
+        <h1>{this.state.blue}</h1>
       </div>
     )
   }
 }
 
 // const App = () => <h1>Hello Eggheads</h1>
+
+class Slider extends React.Component {
+  render() {
+    return (
+      <input type="range"
+        ref="inp"
+        min="0"
+        max="255"
+        onChange={this.props.update} />
+    )
+  }
+}
 
 const Widget = (props) => {
   console.log('props   ', props);
