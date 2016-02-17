c6840de4284d2e29ecfad2c933611cf819a06af6
Choi, Jaehee <jaehee-choi@rsupport.com>
2016-02-17 19:57:04 +0900

    #IMPROVED https://egghead.io/lessons/react-state-basics Example

--- ex15_es6_hello_world/app/components/App.js
+++ ex15_es6_hello_world/app/components/App.js
@@ -1,8 +1,26 @@
 import React from 'react';
 
 class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      txt: 'this is state',
+      cat: 0
+    }
+    this.update = this.update.bind(this);
+  }
+  update(e) {
+    this.setState({
+      txt: e.target.value
+    })
+  }
   render() {
-    return <h1>Hello world</h1>
+    return (
+      <div>
+        <input type="text" onChange={this.update} />
+        <h1>{this.state.txt}</h1>
+      </div>
+    )
   }
 }
 
--- ex15_es6_hello_world/app/index.js
+++ ex15_es6_hello_world/app/index.js
@@ -1,13 +1,12 @@
 import React from 'react';
 import ReactDom from 'react-dom';
-// import App from './components/App';
+import App from './components/App';
 
-class App extends React.Component {
-  render() {
-    let txt = this.props.txt;
-    return <h1>{txt}</h1>
-  }
-}
+// class App extends React.Component {
+//   render() {
+//     return <h1>Hello World</h1>
+//   }
+// }
 
 App.propTypes = {
   txt: React.PropTypes.string,
