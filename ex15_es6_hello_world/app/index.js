4c84e46e552c579da2ac37a6ca689e2de511b193
Choi, Jaehee <jaehee-choi@rsupport.com>
2016-02-18 21:42:26 +0900

    #ADDED https://egghead.io/lessons/developer-tools Example #NOTE NumInput.propTypes = { min: React.PropTypes.number } #NOTE NumInput.defaultProps = { min: 0 }

--- ex15_es6_hello_world/app/index.js
+++ ex15_es6_hello_world/app/index.js
@@ -2,22 +2,7 @@ import React from 'react';
 import ReactDom from 'react-dom';
 import App from './components/App';
 
-// class App extends React.Component {
-//   render() {
-//     return <h1>Hello World</h1>
-//   }
-// }
-
-App.propTypes = {
-  txt: React.PropTypes.string,
-  cat: React.PropTypes.number.isRequired
-}
-
-App.defaultProps = {
-  txt: 'this is the default txt'
-}
-
 ReactDom.render (
-  <App cat={5}/>,
+  <App />,
   document.getElementById('app')
 );
